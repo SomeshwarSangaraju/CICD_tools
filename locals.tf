f63d63d6d977b19e93ad6a701002949a9733dc55
SSR <someshwar.sangaraju19@gmail.com>
2026-04-23 17:04:46 +0530

    jenkins

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals{
-    ami=data.aws_ami.ami_id.value
+    ami=data.aws_ami.ami_id.id
     sonar_ami_id = data.aws_ami.sonarqube.id
     common_name_suffix="${var.project}-${var.environment}"
     common_tags={
